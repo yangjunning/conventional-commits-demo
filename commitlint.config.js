c5f56fb9dfb8ff88e91d128902ce12360744c32a
youngjuning <youngjuning@aliyun.com>
2020-09-28 18:17:36 +0800

    feat: 是的

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,6 +3,8 @@ module.exports = {
   rules: {
     'scope-case': [1, 'always', 'pascal-case'],
     'subject-case': [0],
-    'subject-full-stop': [0]
+    'subject-full-stop': [0],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never']
   }
 };
