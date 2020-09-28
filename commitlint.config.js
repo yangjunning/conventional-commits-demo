081591ae1c56cca41e8494e4ac740f483ba26a34
youngjuning <youngjuning@aliyun.com>
2020-09-28 18:00:40 +0800

    chore: ✨feat: 添加前言

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,5 +4,20 @@ module.exports = {
     'scope-case': [1, 'always', 'pascal-case'],
     'subject-case': [0],
     'subject-full-stop': [0],
+    "type-enum": [
+      2,
+      "always",
+      [
+              "feat✨",
+              "fix🐞",
+              "docs📚",
+              "style💅",
+              "refactor🛠",
+              "perf🐎",
+              "test🏁",
+              "revert⏪",
+              "chore🗯"
+      ]
+]
   }
 };
