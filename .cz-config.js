b9c3fef28202c4e9607dc0c6bf913f3cb917da73
youngjuning <youngjuning@aliyun.com>
2020-09-29 11:34:18 +0800

    chore: 配置prettier自动格式化

--- .cz-config.js
+++ .cz-config.js
@@ -2,51 +2,51 @@ module.exports = {
   types: [
     {
       value: 'feat',
-      name: 'feat:      新功能'
+      name: 'feat:      新功能',
     },
     {
       value: 'fix',
-      name: 'fix:       修复Bug'
+      name: 'fix:       修复Bug',
     },
     {
       value: 'docs',
-      name: 'docs:      文档变动'
+      name: 'docs:      文档变动',
     },
     {
       value: 'style',
-      name: 'style:     不影响代码含义的变化(空白、格式化、缺少分号等)'
+      name: 'style:     不影响代码含义的变化(空白、格式化、缺少分号等)',
     },
     {
       value: 'refactor',
-      name: 'refactor:   重构代码，既不修复错误也不添加功能'
+      name: 'refactor:   重构代码，既不修复错误也不添加功能',
     },
     {
       value: 'perf',
-      name: 'perf:      性能优化'
+      name: 'perf:      性能优化',
     },
     {
       value: 'test',
-      name: 'test:      测试相关'
+      name: 'test:      测试相关',
     },
     {
       value: 'build',
-      name: 'build:       影响构建系统或外部依赖关系的更改'
+      name: 'build:       影响构建系统或外部依赖关系的更改',
     },
     {
       value: 'ci',
-      name: 'ci:          更改持续集成文件和脚本'
+      name: 'ci:          更改持续集成文件和脚本',
     },
     {
       value: 'chore',
-      name: 'chore:       重新打包或更新依赖工具等杂活'
+      name: 'chore:       重新打包或更新依赖工具等杂活',
     },
     {
       value: 'revert',
-      name: 'revert:    Revert to a commit'
+      name: 'revert:    Revert to a commit',
     },
     {
       value: 'wip',
-      name: 'wip:       Work in progress'
+      name: 'wip:       Work in progress',
     },
   ],
   // it needs to match the value for field type. Eg.: 'fix'
@@ -69,7 +69,7 @@ module.exports = {
     body: '请提供一段详细的信息来描述此次更改 (可选). 使用 "|" 来另起一行:\n',
     breaking: '是否有任何 BREAKING CHANGES (可选):\n',
     footer: '是否有任何 ISSUE 可以被此次 Commit 关闭 (可选). E.g.: #31, #34:\n',
-    confirmCommit: '是否确认提交上述 Commit 信息?'
+    confirmCommit: '是否确认提交上述 Commit 信息?',
   },
 
   allowCustomScopes: true,
@@ -77,4 +77,4 @@ module.exports = {
 
   // limit subject length
   subjectLimit: 100,
-}
+};
